8a5571f59b4811d50798c3efa638c2c454cb3e88
inferi0n <rda@apptech-team.ru>
2019-04-29 19:51:04 +0300

    fix: gulp paths

--- tools/paths.js
+++ tools/paths.js
@@ -3,15 +3,15 @@ const paths = {
     html: 'src/html/**/[^_]*.html',
     css: 'src/sass/styles.scss',
     js: 'src/js/*.js',
-    images: 'src/img/**/*.[jpg|jpeg|png|svg]',
-    fonts: 'src/fonts/**/*.[woff|woff2]'
+    images: 'src/img/**/*.{jpg,jpeg,png,svg,webp}',
+    fonts: 'src/fonts/**/*.{woff,woff2}'
   },
   watch: {
     html: 'src/html/**/*.html',
-    css: 'src/sass/*.scss',
-    js: 'src/js/*.js',
-    images: 'src/img/**/*.[jpg|jpeg|png|svg]',
-    fonts: 'src/fonts/**/*.[woff|woff2]'
+    css: 'src/sass/**/*.scss',
+    js: 'src/js/**/**.js',
+    images: 'src/img/**/*.{jpg,jpeg,png,svg,webp}',
+    fonts: 'src/fonts/**/*.{woff,woff2}'
   },
   dist: {
     html: 'dist',
